6a9df95aa2a68be077ccfda72cd4f21cd06db5a7
Alex Tsokurov <me@ximik.net>
2017-01-27 06:47:24 +0300

    rewriting

--- src/index.js
+++ src/index.js
@@ -1,55 +1,76 @@
-var B = require('b_').B;
-var b = B({
-  tailSpace: ' ',
-  elementSeparator: '-',
-  modSeparator: '--',
-  modValueSeparator: '-',
-  classSeparator: ' '
-});
-
-const mods = ["b", "e", "m"];
-
 module.exports = function({ types: t }) {
   return {
     visitor: {
-      JSXOpeningElement(path) {        
-        const params = [],
-              pIndexes = [],
-              scope = path.scope,
-              attributes = path.node.attributes;
-        
-              
+      JSXOpeningElement(path) {
+        const attributes = path.node.attributes;
         if (attributes.length === 0) return;
-        
+
+        let block, element, modifiers, mixins;
+        const scope = path.scope;
         if (scope.block && scope.block.type === 'ArrowFunctionExpression' && scope.parentBlock && scope.parentBlock.type === 'VariableDeclarator') {
-          params[0] = scope.parentBlock.id.name;
+          block = scope.parentBlock.id.name;
         } else if (scope.parent && scope.parent.block && scope.parent.block.type === 'ClassDeclaration') {
-          params[0] = scope.parent.block.id.name;
+          block = scope.parent.block.id.name;
         }
-        
-        let classNameValue, classNameIndex = null;
+
+        let rIndexes = [],
+            cIndex = null;
         for (let i = 0; i < attributes.length; i++) {
           let name = attributes[i].name.name,
-              value = attributes[i].value.value,
-              pIndex = mods.indexOf(name);
+              value = attributes[i].value;
 
-          if (pIndex !== -1) {
-            params[pIndex] = value;
-            pIndexes.push(pIndex);
-          } else if (name === "className") {
-            classNameValue = value;
-            classNameIndex = i;
+          switch (name) {
+            case "b":
+              t.assertStringLiteral(value);
+              rIndexes.push(i);
+              block = value.value;
+              break;
+            case "e":
+              t.assertStringLiteral(value);
+              rIndexes.push(i);
+              element = value.value;
+              break;
+            case "m":
+              t.assertJSXExpressionContainer(value);
+              rIndexes.push(i);
+              modifiers = value.expression;
+              break;
+            case "className":
+              t.assertStringLiteral(value);
+              cIndex = i;
+              mixins = value.value;
+              break;
           }
         }
-        const bem = b.apply(null, params);
-        
-        if (classNameIndex === null) {
-          path.pushContainer('attributes', t.JSXAttribute(t.JSXIdentifier('className'), t.StringLiteral(bem)));
+        if (!block) return;
+
+        let bem;
+        element = element ? block + '__' + element : block;
+        if (modifiers) {
+          bem = t.CallExpression(
+                  t.Identifier('bem'),
+                  [
+                    t.StringLiteral(element),
+                    modifiers,
+                    t.StringLiteral('--')
+                  ]
+                );
+          if (mixins) {
+            bem = t.BinaryExpression('+', bem, t.StringLiteral(mixins));
+          }
         } else {
-          path.get('attributes.' + classNameIndex + '.value').replaceWith(t.StringLiteral(bem + classNameValue));
+          bem = t.StringLiteral(element + ' ' + mixins);
         }
-        pIndexes.forEach((i) => path.get('attributes.' + i).remove());
+        bem = t.JSXExpressionContainer(bem);
+
+        if (cIndex === null) {
+          path.pushContainer('attributes', t.JSXAttribute(t.JSXIdentifier('className'), bem));
+        } else {
+          path.get('attributes.' + cIndex + '.value').replaceWith(bem);
+        }
+        rIndexes.sort().reverse().forEach((i) => path.get('attributes.' + i).remove());
       }
-    }
+    },
+    inherits: require("babel-plugin-syntax-jsx")
   }
 }
